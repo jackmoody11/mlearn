08d406448e3c5a5238e79eb370e2c8e8c5e2ea2c
jackmoody11 <jackmoody@unc.edu>
2018-12-20 15:45:13 -0500

    Add custom SVM code

--- mlearn/svm05.py
+++ mlearn/svm05.py
@@ -1,32 +1,119 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
 import numpy as np
-from sklearn import preprocessing, model_selection, svm
-import pandas as pd
 
 
-df = pd.read_csv('../data/breast-cancer-wisconsin.txt')
-df.replace('?', -99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
-
-# These are all of the values which we believe impact whether or not a tumor is benign or malignant
-# That is why we call them "features"
-X = np.array(df.drop(['class'], 1))
-# These are the results (benign or malignant)
-# We call this our "label." This is what we are predicting (malignant or benign)
-y = np.array(df['class'])
-
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
-clf = svm.SVC()
-clf.fit(X_train, y_train)
-# Measure accuracy
-accuracy = clf.score(X_test, y_test)
-
-print(accuracy)
-
-example_measures = np.array([4, 2, 1, 1, 1, 2, 3, 2, 1], [4, 2, 1, 2, 2, 2, 3, 2, 1])
-example_measures = example_measures.reshape(len(example_measures), -1)
-prediction = clf.predict(example_measures)
-print(prediction)
+style.use('ggplot')
 
 
+class SVM:
+    def __init__(self, visualization=True):
+        self.visualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
 
+    # Train
+    def fit(self, data):
+        self.data = data
+        opt_dict = {}
+        transforms = [[1, 1], [-1, 1], [1, -1], [-1, -1]]
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        step_sizes = [self.max_feature_value * 0.1,
+                      self.max_feature_value * 0.01,
+                      self.max_feature_value * 0.001, ]
+        b_range_multiple = 5
+        b_multiple = 5
+        latest_optimum = self.max_feature_value * 10
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            optimized = False
+            while not optimized:
+                for b in np.arange(-1*(self.max_feature_value * b_range_multiple),
+                                   self.max_feature_value * b_range_multiple,
+                                   step * b_multiple):
+                    for transformation in transforms:
+                        w_t = w * transformation
+                        found_option = True
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi * (np.dot(w_t, xi) + b) >= 1:
+                                    found_option = False
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+                if w[0] < 0:
+                    optimized = True
+                    print("Optimized a step.")
+                else:
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])
+            # ||w|| = [w,b]
+            opt_choice = opt_dict[norms[0]]
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step * 2
+
+    def predict(self, features):
+        classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker='*', c=self.colors[classification])
+        return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color=self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = x.w + b
+        # v = x.w + b
+        # psv = 1
+        # nsv = -1
+        # decision boundary = 0
+        def hyperplane(x, w, b, v):
+            return (-w[0] * x - b + v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+
+        # Positive support vector hyperplane
+        # x.w + b = 1
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2], 'k')
+
+        # Negative support vector hyperplane
+        # x.w + b = -1
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2], 'k')
+
+        # Decision boundary
+        # x.w + b = 0
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2], 'y--')
+
+        plt.show()
+
+
+data_dict = {-1: np.array([[1, 7], [2, 8], [3, 8]]),
+             1: np.array([[5, 1], [6, -1], [7, 3]])}
+
+
+svm = SVM()
+svm.fit(data=data_dict)
+
+predict_us = [[0, 10], [1, 3], [3, 4], [3, 5], [5, 5], [5, 6], [6, -5], [5, 8]]
+for p in predict_us:
+    svm.predict(p)
+svm.visualize()
